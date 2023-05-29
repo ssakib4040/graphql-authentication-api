5a20cfc11a6ab710514ed6025af6299db0b65784
Sadman Sakib <ssakib4040@gmail.com>
2023-05-29 21:13:37 +0600

    Session auth added

--- src/index.ts
+++ src/index.ts
@@ -1,99 +1,106 @@
+import express from "express";
+import http from "http";
+import cors from "cors";
+import session from "express-session";
+
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
-import jwt from "jsonwebtoken";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { json } from "body-parser";
 
-const SECRET_KEY = "your-secret-key";
+const app = express();
+// Express session configuration
+app.use(
+  session({
+    secret: "your-secret-key",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
-// Sample user data for demonstration purposes
 const users = [
-  { id: 1, username: "user123", password: "password123" },
-  { id: 2, username: "anotheruser", password: "pass456" },
+  { username: "john123", password: "password123" },
+  { username: "jane123", password: "password12" },
 ];
 
-const typeDefs = `
+const typeDefs = `#graphql
   type Query {
     hello: String
-    authenticatedData: String
+    getProtectedData: String
   }
 
   type Mutation {
     login(username: String!, password: String!): String
     logout: String
   }
+
 `;
 
 const resolvers = {
   Query: {
-    hello: (a: any, b: any, context: any) => {
-      const { user } = context;
-
-      return "Hello World!";
-    },
-    authenticatedData: (a: any, b: any, { user }: any) => {
-      if (user) {
-        return "This data is only accessible to authenticated users.";
-      } else {
-        throw new Error("Not authenticated");
+    hello: () => "Hello world!",
+    getProtectedData: (a: any, b: any, { req }: any) => {
+      const user = req.session.user;
+      if (!user) {
+        throw new Error("You are not authorized to view this data");
       }
+
+      return "Protected data";
     },
   },
 
   Mutation: {
-    login: (a: any, { username, password }: any) => {
-      // Find user by username and password
+    login: async (_: any, { username, password }: any, { req }: any) => {
+      if (!username || !password) {
+        throw new Error("Please provide username and password");
+      }
+
       const user = users.find(
-        (u) => u.username === username && u.password === password
+        (user) => user.username === username && user.password === password
       );
 
-      if (user) {
-        // Generate a JWT token with user data
-        const token = jwt.sign({ user }, SECRET_KEY);
-
-        // Return the token
-        return token;
-      } else {
+      if (!user) {
         throw new Error("Invalid credentials");
       }
+
+      // console.log(req);
+      req.session.user = user;
+      return "Logged in successfully";
     },
-    logout: () => {
-      // Logging out just means removing the token
+
+    logout: async (_: any, __: any, { req }: any) => {
+      req.session.destroy();
       return "Logged out successfully";
     },
   },
 };
 
-const authenticateUser = (req: any) => {
-  const token = req.headers.authorization || "";
-
-  try {
-    // verify the token
-    const decoded: any = jwt.verify(token, SECRET_KEY);
-
-    // get the user from the decoded token
-    const user = decoded.user;
-
-    // return the decoded user
-    return user;
-  } catch (error) {
-    return null;
-    // throw new Error("Authentication failed!");
-  }
-};
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  nodeEnv: "production",
-});
-
-async function startServer() {
-  const { url } = await startStandaloneServer(server, {
-    context: async ({ req }) => {
-      const user = authenticateUser(req);
-      return { user };
-    },
-  });
-  console.log(`🚀 Server ready at ${url}`);
+interface MyContext {
+  token?: String;
 }
 
-startServer();
+const httpServer = http.createServer(app);
+const server = new ApolloServer<MyContext>({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+
+async function main() {
+  await server.start();
+  app.use(
+    "/graphql",
+    cors<cors.CorsRequest>(),
+    json(),
+    expressMiddleware(server, {
+      context: async ({ req }) => ({ req }),
+    })
+  );
+
+  await new Promise<void>((resolve) =>
+    httpServer.listen({ port: 4000 }, resolve)
+  );
+  console.log(`🚀 Server ready at http://localhost:4000/graphql`);
+}
+
+main();
